beb3a1ac9ef32b4cef24bb1be8441ce0e988ec96
Benjie Gillam <benjie@jemjie.com>
2017-07-29 17:15:39 +0100

    Common header

--- src/templates/page.js
+++ src/templates/page.js
@@ -1,34 +1,8 @@
 import React from "react";
 import Link from "gatsby-link";
 import Helmet from "react-helmet";
-
-const Header = () =>
-  <div
-    style={{
-      background: "#424",
-      marginBottom: "1.45rem",
-    }}
-  >
-    <div
-      style={{
-        margin: "0 auto",
-        maxWidth: 960,
-        padding: "1.45rem 1.0875rem",
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: "white",
-            textDecoration: "none",
-          }}
-        >
-          GraphQL-Build
-        </Link>
-      </h1>
-    </div>
-  </div>;
+import SiteFooter from "../components/SiteFooter";
+import SiteHeader from "../components/SiteHeader";
 
 const Page = ({
   data: { remark: { html, frontmatter: { next, nextText, prev, prevText } } },
@@ -63,36 +37,40 @@ const Page = ({
         crossOrigin="anonymous"
       />
     </Helmet>
-    <Header />
-    <div
+    <SiteHeader />
+    <section
       style={{
         margin: "0 auto",
         maxWidth: 960,
         padding: "0px 1.0875rem 1.45rem",
         paddingTop: 0,
       }}
+      className="page-content"
     >
-      <div className="row">
-        <div
-          dangerouslySetInnerHTML={{ __html: html }}
-          style={{ width: "100%" }}
-        />
-      </div>
-      <div className="row">
-        {prev
-          ? <Link className="btn btn-secondary btn-large" to={prev}>
-              &laquo; {prevText || "Previous"}
-            </Link>
-          : null}
-        <div className="ml-auto">
-          {next
-            ? <Link className="btn btn-primary btn-large" to={next}>
-                {nextText || "Next"} &raquo;
+      <div className="container">
+        <div className="row">
+          <div
+            dangerouslySetInnerHTML={{ __html: html }}
+            style={{ width: "100%" }}
+          />
+        </div>
+        <div className="row">
+          {prev
+            ? <Link className="btn btn-secondary btn-large" to={prev}>
+                &laquo; {prevText || "Previous"}
               </Link>
             : null}
+          <div className="ml-auto">
+            {next
+              ? <Link className="btn btn-primary btn-large" to={next}>
+                  {nextText || "Next"} &raquo;
+                </Link>
+              : null}
+          </div>
         </div>
       </div>
-    </div>
+    </section>
+    <SiteFooter />
   </div>;
 
 export default Page;
