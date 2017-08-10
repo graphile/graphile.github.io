aa109aad4178ec4739e266165e0624b336111852
Benjie Gillam <benjie@jemjie.com>
2017-08-10 10:53:18 +0100

    Load mailchimp code after page load

--- src/pages/index.md
+++ src/pages/index.md
@@ -254,7 +254,7 @@ main();
 <div class='row justify-content-center'>
 <div class='col-12 col-md-8 text-center'>
 <!-- Begin MailChimp Signup Form -->
-<link href="//cdn-images.mailchimp.com/embedcode/classic-10_7.css" rel="stylesheet" type="text/css">
+<link href="//cdn-images.mailchimp.com/embedcode/classic-10_7.css" rel="stylesheet" type="text/css" />
 <style type="text/css">
 	#mc_embed_signup{background:#fff; clear:left; font:14px Helvetica,Arial,sans-serif; }
 	/* Add your own MailChimp form style overrides in your site stylesheet or in this style block.
@@ -282,7 +282,6 @@ id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validat
   </div>
 </form>
 </div>
-<script type='text/javascript' src='//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js'></script><script type='text/javascript'>(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';fnames[1]='FNAME';ftypes[1]='text';fnames[2]='LNAME';ftypes[2]='text';fnames[3]='BIRTHDAY';ftypes[3]='birthday';}(jQuery));var $mcj = jQuery.noConflict(true);</script>
 <!--End mc_embed_signup-->
 </div>
 </div>
--- src/templates/marketing.js
+++ src/templates/marketing.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import PropTypes from "prop-types";
 import Helmet from "react-helmet";
 import SiteFooter from "../components/SiteFooter";
@@ -6,30 +6,64 @@ import SiteHeader from "../components/SiteHeader";
 
 import "prismjs/themes/prism-solarizedlight.css";
 
-const Marketing = ({
-  data: { remark: { html, frontmatter: { next, nextText, prev, prevText } } },
-  location,
-}) =>
-  <div className="template-marketing">
-    <Helmet
-      title="Graphile | Powerful, Extensible and Performant GraphQL APIs Rapidly"
-      meta={[
-        {
-          name: "description",
-          content:
-            "Utilities to build powerful, performant and extensible GraphQL APIs rapidly",
-        },
-        {
-          name: "keywords",
-          content:
-            "GraphQL, API, Graph, PostgreSQL, PostGraphQL, server, plugins, introspection, reflection",
-        },
-      ]}
-    />
-    <SiteHeader location={location} />
-    <div className="page-content" dangerouslySetInnerHTML={{ __html: html }} />
-    <SiteFooter />
-  </div>;
+class Marketing extends Component {
+  state = {
+    mailchimp: false,
+  };
+  componentDidMount() {
+    setTimeout(this.mailchimpScript, 0);
+  }
+  mailchimpScript = () => {
+    this.setState({ mailchimp: true });
+  };
+  render() {
+    const {
+      data: {
+        remark: { html, frontmatter: { next, nextText, prev, prevText } },
+      },
+      location,
+    } = this.props;
+    return (
+      <div className="template-marketing">
+        <Helmet
+          title="Graphile | Powerful, Extensible and Performant GraphQL APIs Rapidly"
+          meta={[
+            {
+              name: "description",
+              content:
+                "Utilities to build powerful, performant and extensible GraphQL APIs rapidly",
+            },
+            {
+              name: "keywords",
+              content:
+                "GraphQL, API, Graph, PostgreSQL, PostGraphQL, server, plugins, introspection, reflection",
+            },
+          ]}
+        >
+          {this.state.mailchimp &&
+            <script
+              type="text/javascript"
+              defer
+              src="//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js"
+            />}
+          {this.state.mailchimp &&
+            <script
+              type="text/javascript"
+              dangerouslySetInnerHTML={{
+                __html: `(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';fnames[1]='FNAME';ftypes[1]='text';fnames[2]='LNAME';ftypes[2]='text';fnames[3]='BIRTHDAY';ftypes[3]='birthday';}(jQuery));var $mcj = jQuery.noConflict(true);`,
+              }}
+            />}
+        </Helmet>
+        <SiteHeader location={location} />
+        <div
+          className="page-content"
+          dangerouslySetInnerHTML={{ __html: html }}
+        />
+        <SiteFooter />
+      </div>
+    );
+  }
+}
 
 Marketing.propTypes = {
   children: PropTypes.func,
