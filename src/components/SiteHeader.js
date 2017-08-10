a4db7102b7b4cd8ee0163d351ae3c63a4921b1c6
Jof Arnold <public@jofarnold.com>
2017-08-10 20:09:40 +0100

    Add CSS only collapsable menu.

--- src/components/SiteHeader.js
+++ src/components/SiteHeader.js
@@ -5,62 +5,69 @@ export default ({ location }) =>
   <header className="content">
     <nav className="navbar">
       <div className="container">
-        <button
+        <input
           className="navbar-toggler"
-          type="button"
+          type="checkbox"
+          id="toggle"
           data-toggle="collapse"
           data-target="#navbarSupportedContent"
           aria-controls="navbarSupportedContent"
           aria-expanded="false"
           aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon fa fa-bars" /> Menu
-        </button>
-        <ul className="navbar-nav">
-          <li className="nav-item">
-            <Link
-              className={`nav-link ${location.pathname.match(/^\/$/)
-                ? "active"
-                : ""}`}
-              to="/"
-            >
-              <span className="fa fa-home" /> <span className="home">Home</span>
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link
-              className={`nav-link ${location.pathname.match(
-                /^\/graphile-build(\/|$)/
-              )
-                ? "active"
-                : ""}`}
-              to="/graphile-build/getting-started/"
-            >
-              graphile-build
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link
-              className={`nav-link ${location.pathname.match(
-                /^\/graphile-build-pg(\/|$)/
-              )
-                ? "active"
-                : ""}`}
-              to="/graphile-build-pg/introduction/"
-            >
-              graphile-build-pg
-            </Link>
-          </li>
-          <li className="nav-item ml-auto">
-            <a
-              className="nav-github-link nav-link"
-              href="https://github.com/graphile/graphile-build"
-            >
-              <span className="fa fa-github" />{" "}
-              <span className="github">Github</span>
-            </a>
-          </li>
-        </ul>
+        />
+        <div className="nav-controls">
+          <div className="navbar-crosses">
+            <span className="line line-1"> </span>
+            <span className="line line-2"> </span>
+            <span className="line line-3"> </span>
+          </div>
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <Link
+                className={`nav-link ${location.pathname.match(/^\/$/)
+                  ? "active"
+                  : ""}`}
+                to="/"
+              >
+                <span className="fa fa-home" />{" "}
+                <span className="home">Home</span>
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                className={`nav-link ${location.pathname.match(
+                  /^\/graphile-build(\/|$)/
+                )
+                  ? "active"
+                  : ""}`}
+                to="/graphile-build/getting-started/"
+              >
+                graphile-build
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                className={`nav-link ${location.pathname.match(
+                  /^\/graphile-build-pg(\/|$)/
+                )
+                  ? "active"
+                  : ""}`}
+                to="/graphile-build-pg/introduction/"
+              >
+                graphile-build-pg
+              </Link>
+            </li>
+            <li className="nav-item ml-auto">
+              <a
+                className="nav-github-link nav-link"
+                href="https://github.com/graphile/graphile-build"
+              >
+                <span className="fa fa-github" />{" "}
+                <span className="github">Github</span>
+              </a>
+            </li>
+          </ul>
+        </div>
       </div>
     </nav>
   </header>;
