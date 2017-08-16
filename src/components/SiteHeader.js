d5880d323987ad245d2626caca70bc9f782e4ee9
Benjie Gillam <benjie@jemjie.com>
2017-08-16 19:44:56 +0100

    Make DocSearch continue to work when a new search element is built

--- src/components/SiteHeader.js
+++ src/components/SiteHeader.js
@@ -1,79 +1,120 @@
 import React from "react";
 import Link from "gatsby-link";
+import { withRouter } from "react-router-dom";
 
-export default ({ location }) =>
-  <header className="content">
-    <nav className="navbar">
-      <div className="container">
-        <input
-          className="navbar-toggler"
-          type="checkbox"
-          id="toggle"
-          data-toggle="collapse"
-          data-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        />
-        <div className="nav-controls">
-          <div className="navbar-crosses">
-            <span className="line line-1"> </span>
-            <span className="line line-2"> </span>
-            <span className="line line-3"> </span>
-          </div>
-          <ul className="navbar-nav">
-            <li className="navbar-item navbar-item--left">
-              <Link
-                className={`nav-link ${location.pathname.match(/^\/$/)
-                  ? "active"
-                  : ""}`}
-                to="/"
-              >
-                <span className="fa fa-home" />{" "}
-                <span className="home">Home</span>
-              </Link>
-            </li>
-            <li className="navbar-item navbar-item--left">
-              <Link
-                className={`nav-link ${location.pathname.match(
-                  /^\/postgraphile(\/|$)/
-                )
-                  ? "active"
-                  : ""}`}
-                to="/postgraphile/"
-              >
-                PostGraphile
-              </Link>
-            </li>
-            <li className="navbar-item navbar-item--left">
-              <Link
-                className={`nav-link ${location.pathname.match(
-                  /^\/graphile-build(\/|$)/
-                )
-                  ? "active"
-                  : ""}`}
-                to="/graphile-build/"
-              >
-                Graphile Build
-              </Link>
-            </li>
-            <li className="navbar-item ml-auto navbar-item navbar-item--right">
-              <span className="searchbox-container">
-                <input id="search-box" placeholder="Search" />
-                <span className="fa fa-search searchbox-search" />
-              </span>
-            </li>
-            <li className="navbar-item navbar-item--right">
-              <a
-                className="nav-github-link nav-link"
-                href="https://github.com/graphile/graphile-build"
-              >
-                <span className="fa fa-github" />{" "}
-                <span className="github">Github</span>
-              </a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
-  </header>;
+function enableSearch(history) {
+  if (typeof docsearch === "undefined") {
+    return;
+  }
+  docsearch({
+    apiKey: "b8bae02e7bf22b05801b361ea00f9bf5",
+    indexName: "graphile",
+    inputSelector: "#search-box",
+    debug: false,
+    handleSelected: (input, event, suggestion) => {
+      const url = suggestion.url;
+      const path = url.replace(/^https?:\/\/[^/]*/, "");
+      history.push(path);
+    },
+  });
+}
+
+export default withRouter(
+  class SiteHeader extends React.Component {
+    handleSearchBoxRef = () => {
+      document.addEventListener(
+        "DOMContentLoaded",
+        enableSearch.bind(null, this.props.history)
+      );
+      document.addEventListener(
+        "load",
+        enableSearch.bind(null, this.props.history)
+      );
+      enableSearch(this.props.history);
+    };
+    render() {
+      const { location } = this.props;
+      return (
+        <header className="content">
+          <nav className="navbar">
+            <div className="container">
+              <input
+                className="navbar-toggler"
+                type="checkbox"
+                id="toggle"
+                data-toggle="collapse"
+                data-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+              />
+              <div className="nav-controls">
+                <div className="navbar-crosses">
+                  <span className="line line-1"> </span>
+                  <span className="line line-2"> </span>
+                  <span className="line line-3"> </span>
+                </div>
+                <ul className="navbar-nav">
+                  <li className="navbar-item navbar-item--left">
+                    <Link
+                      className={`nav-link ${location.pathname.match(/^\/$/)
+                        ? "active"
+                        : ""}`}
+                      to="/"
+                    >
+                      <span className="fa fa-home" />{" "}
+                      <span className="home">Home</span>
+                    </Link>
+                  </li>
+                  <li className="navbar-item navbar-item--left">
+                    <Link
+                      className={`nav-link ${location.pathname.match(
+                        /^\/postgraphile(\/|$)/
+                      )
+                        ? "active"
+                        : ""}`}
+                      to="/postgraphile/"
+                    >
+                      PostGraphile
+                    </Link>
+                  </li>
+                  <li className="navbar-item navbar-item--left">
+                    <Link
+                      className={`nav-link ${location.pathname.match(
+                        /^\/graphile-build(\/|$)/
+                      )
+                        ? "active"
+                        : ""}`}
+                      to="/graphile-build/"
+                    >
+                      Graphile Build
+                    </Link>
+                  </li>
+                  <li className="navbar-item ml-auto navbar-item navbar-item--right">
+                    <span className="searchbox-container">
+                      <input
+                        id="search-box"
+                        placeholder="Search"
+                        ref={this.handleSearchBoxRef}
+                      />
+                      <span className="fa fa-search searchbox-search" />
+                    </span>
+                  </li>
+                  <li className="navbar-item navbar-item--right">
+                    <a
+                      className="nav-github-link nav-link"
+                      href="https://github.com/graphile/graphile-build"
+                    >
+                      <span className="fa fa-github" />{" "}
+                      <span className="github">Github</span>
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </nav>
+        </header>
+      );
+    }
+  }
+);
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,43 +1,11 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
-import { withRouter } from "react-router-dom";
 
 import "prismjs/themes/prism-solarizedlight.css";
 import "./index.scss";
 import "./flexgrid.scss";
 
-let initialised = false;
-
-function init(history) {
-  if (typeof docsearch === "undefined") {
-    return;
-  }
-  if (!initialised) {
-    initialised = true;
-
-    docsearch({
-      apiKey: "b8bae02e7bf22b05801b361ea00f9bf5",
-      indexName: "graphile",
-      inputSelector: "#search-box",
-      debug: false,
-      handleSelected: (input, event, suggestion) => {
-        const url = suggestion.url;
-        const path = url.replace(/^https?:\/\/[^/]*/, "");
-        history.push(path);
-      },
-    });
-  }
-}
-
 class TemplateWrapper extends Component {
-  componentDidMount() {
-    document.addEventListener(
-      "DOMContentLoaded",
-      init.bind(null, this.props.history)
-    );
-    document.addEventListener("load", init.bind(null, this.props.history));
-    init(this.props.history);
-  }
   render() {
     const { children, ...restOfProps } = this.props;
     return (
@@ -50,7 +18,6 @@ class TemplateWrapper extends Component {
 
 TemplateWrapper.propTypes = {
   children: PropTypes.func,
-  history: PropTypes.object.isRequired,
 };
 
-export default withRouter(TemplateWrapper);
+export default TemplateWrapper;
