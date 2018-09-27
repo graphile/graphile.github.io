62c84f82f85769e0fee77f4a61104831639b5cc4
Benjie Gillam <benjie@jemjie.com>
2018-09-27 12:01:40 +0100

    Improve link checker

--- link-check.js
+++ link-check.js
@@ -1,12 +1,18 @@
-const fs = require('fs');
-const glob = require('glob');
+/* eslint-disable no-console */
+const fs = require("fs");
+const path = require("path");
+const glob = require("glob");
+const chalk = require("chalk");
 
 const base = `${__dirname}/public`;
 const files = glob.sync(`${base}/**/*.html`);
-const validLinks = files.map(f => f.substr(base.length).replace(/index.html$/, ''))
+const validLinks = files.map(f =>
+  f.substr(base.length).replace(/index.html$/, "")
+);
 
 function allMatches(str, regex) {
   const all = [];
+  // eslint-disable-next-line no-constant-condition
   while (true) {
     const matches = regex.exec(str);
     if (!matches) break;
@@ -18,15 +24,30 @@ function allMatches(str, regex) {
 let invalid = 0;
 
 for (const file of files) {
-  const contents = fs.readFileSync(file, 'utf8');
+  const filePretty = chalk.bold(
+    path
+      .relative(__dirname, file)
+      .replace(/^public/, "")
+      .replace(/index.html$/, "")
+  );
+  const contents = fs.readFileSync(file, "utf8");
+  if (contents.indexOf("Postgraphile") >= 0) {
+    invalid++;
+    console.error(
+      `${filePretty} mentions 'Postgraphile'; please change to 'PostGraphile' or 'postgraphile' for consistency.`
+    );
+  }
   const links = allMatches(contents, /href="([^"]+)"/g);
   for (const link of links) {
-    const trimmed = link.replace(/[?#].*$/, '');
+    const trimmed = link.replace(/[?#].*$/, "");
     if (trimmed.match(/\.(js|css|png|svg)$/)) {
       // Meh, resources
       continue;
     }
-    if (trimmed.match(/^(https?:)?\/\//) && (trimmed.match(/github\.com/) || !trimmed.match(/graphile\.(org|meh)/))) {
+    if (
+      trimmed.match(/^(https?:)?\/\//) &&
+      (trimmed.match(/github\.com/) || !trimmed.match(/graphile\.(org|meh)/))
+    ) {
       // Absolute, continue
       continue;
     }
@@ -34,7 +55,7 @@ for (const file of files) {
       // mailto:, continue
       continue;
     }
-    if (trimmed === '') {
+    if (trimmed === "") {
       // Anchor link
       continue;
     }
@@ -43,11 +64,12 @@ for (const file of files) {
       continue;
     }
     invalid++;
-    console.error(`${file} has invalid link to '${link}'`);
+    console.error(`${filePretty} has invalid link to '${link}'`);
   }
 }
 
 if (invalid > 0) {
-  console.log(`${invalid} invalid links found`);
+  console.log();
+  console.log(`${invalid} errors found 😔`);
   process.exit(1);
 }
