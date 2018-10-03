88bba95619ebfe11803e8b8c1461758bd3ae1ffe
Benjie Gillam <benjie@jemjie.com>
2018-10-03 10:16:16 +0100

    Diff for float plugins, tidier diffs

--- examples/functions/0300_custom_mutations/insert-multiple-records.sql
+++ examples/functions/0300_custom_mutations/insert-multiple-records.sql
@@ -17,27 +17,6 @@
  */
 
 /**
- * These are the tables we're using in this example
- */
-create table quiz (
-  id serial primary key,
-  name text not null
-);
-
-create table quiz_entry (
-  id serial primary key,
-  user_id int not null references users(id),
-  quiz_id int not null references quiz(id)
-);
-
-create table quiz_entry_answer (
-  id serial primary key,
-  quiz_entry_id int not null references quiz_entry(id),
-  question text not null,
-  answer int
-);
-
-/**
  * This type is used for input in the mutation
  */
 create type quiz_entry_input as (
