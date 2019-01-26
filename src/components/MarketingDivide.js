b8ec60679fce557202bf26f638f4291b469a850e
Benjie Gillam <benjie@jemjie.com>
2019-01-26 13:39:39 +0000

    Transparent-capable

--- src/components/MarketingDivide.js
+++ src/components/MarketingDivide.js
@@ -2,10 +2,13 @@ import React from "react";
 import COLOURS from "./colours";
 
 export default function MarketingDivide({ from, to, via, down }) {
-  const startX = down ? 0 : 1000;
-  const endX = down ? 1000 : 0;
-  const startY = 0;
-  const endY = 200;
+  const invert = to === "nodes";
+  const startX = down ^ invert ? 0 : 1000;
+  const endX = down ^ invert ? 1000 : 0;
+  const startY = invert ? 200 : 0;
+  const endY = invert ? 0 : 200;
+  const transparentBg = to === "nodes" || from === "nodes";
+  const otherColor = invert ? from : to;
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
@@ -16,23 +19,25 @@ export default function MarketingDivide({ from, to, via, down }) {
       preserveAspectRatio="none"
       style={{ display: "block" }}
     >
-      <path
-        d="M0,0 L1000,0 L1000,200 L0,200 Z"
-        fill={COLOURS[from]}
-        strokeLinejoin="miter"
-      />
+      {transparentBg ? null : (
+        <path
+          d="M0,0 L1000,0 L1000,200 L0,200 Z"
+          fill={COLOURS[from]}
+          strokeLinejoin="miter"
+        />
+      )}
       <path
         d={`M${endX},${endY} L${endX},${
           endY > 0 ? endY * 0.95 : startY * 0.05
         } L${startX},${startY} L${startX},${endY} Z`}
-        fill={COLOURS[via || to]}
+        fill={COLOURS[via || otherColor]}
         strokeLinejoin="miter"
       />
       {via ? (
         <path
           d={`M${endX},${endY} L${startX},${(endY || startY) /
             2} L${startX},${endY} Z`}
-          fill={COLOURS[to]}
+          fill={COLOURS[otherColor]}
           strokeLinejoin="miter"
         />
       ) : null}
