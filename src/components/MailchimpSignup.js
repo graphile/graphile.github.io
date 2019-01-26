37468bc8609b224b24ead4eed95a0cf3f6511206
Benjie Gillam <benjie@jemjie.com>
2019-01-26 18:19:34 +0000

    Mailchimp needs jQuery?

--- src/components/MailchimpSignup.js
+++ src/components/MailchimpSignup.js
@@ -76,6 +76,7 @@ export default function MailchimpSignup() {
         defer
         src="//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js"
       />
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js" />
       <script
         type="text/javascript"
         dangerouslySetInnerHTML={{
--- src/templates/marketing.js
+++ src/templates/marketing.js
@@ -60,10 +60,13 @@ class Marketing extends Component {
               />
             )}
             {this.state.mailchimp && (
+              <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js" />
+            )}
+            {this.state.mailchimp && (
               <script
                 type="text/javascript"
                 dangerouslySetInnerHTML={{
-                  __html: `(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';fnames[1]='FNAME';ftypes[1]='text';fnames[2]='LNAME';ftypes[2]='text';fnames[3]='BIRTHDAY';ftypes[3]='birthday';}(window.jQuery));var $mcj = jQuery.noConflict(true);`,
+                  __html: `(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';fnames[1]='FNAME';ftypes[1]='text';fnames[2]='LNAME';ftypes[2]='text';fnames[3]='BIRTHDAY';ftypes[3]='birthday';}(jQuery));var $mcj = jQuery.noConflict(true);`,
                 }}
               />
             )}
