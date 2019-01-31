8b73ba524396b77639b36acd851fb6b6f3c61562
Benjie Gillam <benjie@jemjie.com>
2019-01-31 12:37:29 +0000

    Update sponsor page

--- src/components/MarketingSection.js
+++ src/components/MarketingSection.js
@@ -1,15 +1,26 @@
 import React from "react";
 import COLOURS, { TEXT } from "./colours";
 
-export default function MarketingSection({ className = "", children, bg }) {
+export default function MarketingSection({
+  className = "",
+  children,
+  bg,
+  left,
+  maxWidth,
+}) {
   return (
     <div
       className={
-        className + ` pv3 ma0 marketing-section marketing-section-${bg} tc`
+        className +
+        ` pv3 ma0 marketing-section marketing-section-${bg} ${
+          left ? "tl" : "tc"
+        }`
       }
       style={{ backgroundColor: COLOURS[bg], color: TEXT[bg] }}
     >
-      <div className="container">{children}</div>
+      <div className="container" style={maxWidth ? { maxWidth: "48rem" } : {}}>
+        {children}
+      </div>
     </div>
   );
 }
---
+++ src/components/MarketingSponsor.js
@@ -0,0 +1,71 @@
+import React from "react";
+import { upperFirst } from "lodash";
+import fallbackAvatar from "../images/avatar.svg";
+
+const LevelContext = React.createContext(null);
+
+export default function MarketingSponsors({ level, children }) {
+  return (
+    <LevelContext.Provider value={level}>
+      <div>
+        <div
+          className="flex"
+          style={{
+            justifyItems: "stretch",
+            justifyContent: "space-around",
+            alignItems: "stretch",
+            flexWrap: "wrap",
+
+            fontSize:
+              level === "featured"
+                ? "1.6rem"
+                : level === "leaders"
+                ? "1.25rem"
+                : "1rem",
+          }}
+        >
+          {children}
+        </div>
+      </div>
+    </LevelContext.Provider>
+  );
+}
+
+function Avatar({ src }) {
+  return (
+    <img
+      style={{ borderRadius: "10rem", width: "5rem", height: "5rem" }}
+      src={src || fallbackAvatar}
+    />
+  );
+}
+
+export class Sponsor extends React.Component {
+  static contextType = LevelContext;
+  render() {
+    const { name, contributor, avatar } = this.props;
+    const level = this.context;
+    const showAvatar = level === "featured" || level === "leaders";
+    return (
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          justifyItems: "center",
+          justifyContent: "space-evenly",
+          alignItems: "center",
+
+          flex: `0 0 ${showAvatar ? "14em" : "12em"}`,
+          backgroundColor: "white",
+          color: "#1b3955",
+          margin: "0.5em",
+          borderRadius: 0,
+          minHeight: showAvatar ? "8em" : "3em",
+        }}
+      >
+        {showAvatar ? <Avatar src={avatar} /> : null}
+        <span>{name}</span>
+      </div>
+    );
+  }
+}
