abce0d51184a61c13f299b3dcb9ca61f31e8847d
Jem Gillam <6413628+jemgillam@users.noreply.github.com>
2019-09-23 12:40:33 +0100

    Add Changelog to footer

--- src/components/SiteFooter.js
+++ src/components/SiteFooter.js
@@ -22,6 +22,11 @@ const SiteFooter = () => {
                   Community Plugins
                 </Link>
               </li>
+              <li>
+                <Link to="https://github.com/graphile/postgraphile/releases">
+                  <i className="fab fa-github" /> Changelog
+                </Link>
+              </li>
             </ul>
           </div>
           <div className="col-xs-12 col-md-2 nested-list-reset ">
