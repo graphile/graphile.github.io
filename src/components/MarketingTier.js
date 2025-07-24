db247617afd8858f60384a100531c752510d0cca
Jem Gillam <6413628+jemgillam@users.noreply.github.com>
2025-07-24 17:39:51 +0100

    initial tiers added

---
+++ src/components/MarketingTier.js
@@ -0,0 +1,56 @@
+import React from "react";
+
+export default function MarketingTiers({ children }) {
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "row",
+        gap: "1rem",
+        alignItems: "stretch",
+      }}
+    >
+      {children}
+    </div>
+  );
+}
+
+export class Tier extends React.Component {
+  render() {
+    const { name, price, tagline, href, description } = this.props;
+
+    return (
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          justifyItems: "space-between",
+          alignContent: "flex-start",
+          backgroundColor: "white",
+          margin: "0.5em",
+          padding: "1rem",
+          borderRadius: "12px",
+          border: "4px solid #1b1b3d",
+          color: "#1b3955",
+          flex: "0 0 33%",
+        }}
+      >
+        <h3>{name}</h3>
+        <span>
+          <span style={{ fontSize: "1.5rem" }}>{price}</span>{" "}
+          <span style={{ fontSize: "0.8rem" }}>/month</span>
+        </span>
+        <div class="tc">
+          <a class="button--solid" href={href}>
+            Join on GitHub Sponsors{" "}
+            <span class="fas fa-fw fa-external-link-square-alt" />
+          </a>
+        </div>
+        <h4>{tagline}</h4>
+        <div>
+          <span>{description}</span>
+        </div>
+      </div>
+    );
+  }
+}
