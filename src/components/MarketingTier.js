d62044d595e8ca8cc2fbdd0b211086be186fb6cc
Benjie Gillam <benjie@jemjie.com>
2025-09-24 17:20:26 +0100

    Mess with styles

--- src/components/MarketingTier.js
+++ src/components/MarketingTier.js
@@ -2,46 +2,27 @@ import React from "react";
 
 export default function MarketingTiers({ children }) {
   return (
-    <div
-      className="tier-row"
-      style={{
-        display: "flex",
-        flexDirection: "row",
-        gap: "1rem",
-        alignItems: "stretch",
-      }}
-    >
-      {children}
+    <div className="tier-row">
+      <div className="tier-item-container">
+        {children.flatMap((c, i) =>
+          i === 0 ? [c] : [<div className="tier-break" />, c]
+        )}
+      </div>
     </div>
   );
 }
 
 export class Tier extends React.Component {
   render() {
-    const { name, price, tagline, href, description } = this.props;
+    const { name, price, tagline, href, description, highlight } = this.props;
 
     return (
-      <div
-        className="tier"
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          justifyItems: "space-between",
-          alignContent: "flex-start",
-          backgroundColor: "white",
-          margin: "0.5em",
-          padding: "1rem",
-          borderRadius: "12px",
-          border: "4px solid #1b1b3d",
-          color: "#1b3955",
-          flex: "0 0 33%",
-        }}
-      >
+      <div className={`tier ${highlight ? "highlight" : ""}`}>
         <h3>{name}</h3>
-        <span>
+        <div className="price-line">
           <span style={{ fontSize: "1.5rem" }}>{price}</span>{" "}
           <span style={{ fontSize: "0.8rem" }}>/month</span>
-        </span>
+        </div>
         <div className="tc">
           <a className="button--solid" href={href}>
             Join on GitHub Sponsors{" "}
