98509d6d5a7b49500d59e579d6eb76c7824db3db
Benjie Gillam <benjie@jemjie.com>
2026-03-23 21:42:28 +0000

    And for build website

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -10,6 +10,16 @@ function checkRedirect(inPath) {
       window.location.replace(`https://postgraphile.org${parts.join("/")}`);
     }
   }
+  if (path === "/graphile-build" || path.startsWith("/graphile-build/")) {
+    const parts = path.split("/");
+    if (!parts[2]) {
+      window.location.replace(`https://build.graphile.org`);
+    } else {
+      if (parts[2] === "introduction") parts.length = 2;
+      parts.splice(2, 0, "4");
+      window.location.replace(`https://build.graphile.org${parts.join("/")}`);
+    }
+  }
 }
 
 exports.onRouteUpdate = ({ location, prevLocation }) => {
